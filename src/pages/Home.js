a9e9cfc4062420e3340d2ecaf5b59c25442dab34
Kelly Owoju <kelliebrighty@gmail.com>
2022-04-17 07:13:05 +0100

    my boilerplate commit here

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <>
+      <Link to="/rentals"> Rentals </Link>
+    </>
+  );
+};
+
+export default Home;
